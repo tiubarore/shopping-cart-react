98e493fcf3ea7f5320e9699cfb9cad1bb4343a5f
Vincenzo Nicassio <tiubarore93@gmail.com>
2025-08-22 12:29:14 +0200

    fixed loading bug

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,8 +5,10 @@ const ProductList = () => {
   const { products, loading, error } = useProducts();
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
-      {loading && <p>Loading..</p>}
-      {error && <p className="text-red font-semibold text-2xl">{error}</p>}
+      {loading && <p className="text-center text-2xl text-black">Loading..</p>}
+      {error && (
+        <p className="text-red font-semibold text-2xl text-center">{error}</p>
+      )}
       {products.map((product) => (
         <ProductCard product={product} key={product.id} />
       ))}
